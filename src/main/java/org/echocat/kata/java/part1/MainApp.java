820e3e7c1968e5965f135deb6ba3885555b0d0a1
Jan Kurella <jan.kurella@meteogroup.com>
2020-12-11 20:42:03 +0100

    deduplicate

--- src/main/java/org/echocat/kata/java/part1/MainApp.java
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -94,7 +94,7 @@ public class MainApp {
         final String[] details = csvLine.split(SEP);
         // TODO evaluate format stability and add safeguards
         return new Book(details[0],
-                        Arrays.stream(details[2].split(",")).map((authorMail)->fetchAuthor(authorMail, authors)).collect(Collectors.toSet()),
+                        parseAuthorRefs(authors, details[2]),
                         details[1],
                         details[3]);
     }
@@ -103,16 +103,24 @@ public class MainApp {
         final String[] details = csvLine.split(SEP);
         // TODO evaluate format stability and add safeguards
         return new Magazine(details[0],
-                        Arrays.stream(details[2].split(",")).map((authorMail)->fetchAuthor(authorMail, authors)).collect(Collectors.toSet()),
+                            parseAuthorRefs(authors, details[2]),
                         details[1],
                         new SimpleDateFormat("dd.MM.yyyy").parse(details[3]));
     }
 
-
-    private static  Author fetchAuthor(String authorEMail, Set<Author> authors) {
-        // TODO unsauber ohne check
-        // TODO convert authors to HashMap for lookup
-        return authors.stream().filter((author -> author.getEMail().equals(authorEMail))).findFirst().orElseThrow();
+    /**
+     * @param authors known authors
+     * @param authorRefString comma separated list of author mails
+     * @return lis of authors
+     * @throws  NoSuchElementException if the author’s mail cannot be mached to the known authors
+     */
+    private static Set<Author> parseAuthorRefs(Set<Author> authors, String authorRefString) {
+        return Arrays.stream(authorRefString.split(","))
+            .map((authorMail) -> authors.stream()
+                .filter((author -> author.getEMail().equals(authorMail)))
+                .findFirst()
+                .orElseThrow())
+            .collect(Collectors.toSet());
     }
 
     private static Author parseAuthor(String nextLine) {
