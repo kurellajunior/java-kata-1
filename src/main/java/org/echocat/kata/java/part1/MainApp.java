3557015330a1d9d4efed7949c777fea949d565bb
Jan Kurella <jan.kurella@meteogroup.com>
2020-12-11 11:34:16 +0100

    prepare for parsing

--- src/main/java/org/echocat/kata/java/part1/MainApp.java
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -9,27 +9,43 @@ public class MainApp {
 
     public static void main(String[] args) {
         final Set<Author> authors = readAuthors();
-        final Set<PrintMedia> library = readAllMedia();
+        final Set<? extends PrintMedia> library = readAllMedia();
 
     }
 
-    private static Set<PrintMedia> readAllMedia() {
-        Set<PrintMedia> media = readBooks();
+    private static Set<? extends PrintMedia> readAllMedia() {
+        Set<PrintMedia> media = new HashSet<>(readBooks());
         media.addAll(readMagazines());
         return media;
     }
 
-    private static Set<? extends PrintMedia> readMagazines() {
-        return new HashSet<>();
+    private static Set<? extends PrintMedia> readBooks() {
+        Set<Book> books = new HashSet<>();
+        try (Scanner scanner = getScannerFor("/org/echocat/kata/java/part1/data/books.csv")) {
+            // skip header, use later for dynamic column order
+            if (scanner.hasNextLine()) scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                books.add(parseBook(scanner.nextLine()));
+            }
+        }
+        return books;
     }
 
-    private static Set<PrintMedia> readBooks() {
-        return new HashSet<>();
+    private static Set<? extends PrintMedia> readMagazines() {
+        Set<Magazine> magazines = new HashSet<>();
+        try (Scanner scanner = getScannerFor("/org/echocat/kata/java/part1/data/magazines.csv")) {
+            // skip header, use later for dynamic column order
+            if (scanner.hasNextLine()) scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                magazines.add(parseMagazine(scanner.nextLine()));
+            }
+        }
+        return magazines;
     }
 
     protected static Set<Author> readAuthors() {
         Set<Author> authors = new HashSet<>();
-        try (Scanner scanner = new Scanner(MainApp.class.getResourceAsStream("/org/echocat/kata/java/part1/data/authors.csv"), StandardCharsets.UTF_8)) {
+        try (Scanner scanner = getScannerFor("/org/echocat/kata/java/part1/data/authors.csv")) {
             // skip header, use later for dynamic column order
             if (scanner.hasNextLine()) scanner.nextLine();
             while (scanner.hasNextLine()) {
@@ -39,11 +55,23 @@ public class MainApp {
         return authors;
     }
 
+    private static Book parseBook(String csvLine) {
+        return null;
+    }
+
+    private static Magazine parseMagazine(String csvLine) {
+        return null;
+    }
+
     private static Author parseAuthor(String nextLine) {
         final String sep = ";";
         final String[] details = nextLine.split(sep);
         // TODO evaluate format stability and add safeguards
         return new Author(details[1], details[2], details[0]);
+    }
+
+    private static Scanner getScannerFor(String dataSource) {
+        return new Scanner(MainApp.class.getResourceAsStream(dataSource), StandardCharsets.UTF_8);
     }
 
 }
